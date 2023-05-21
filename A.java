5c6358ca602d14443ff17780a7c7f55b1b8c9c3c
Gabseek <125920170+Gabseek@users.noreply.github.com>
2023-05-21 14:02:20 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A {
+    private int A1;
+    private float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA1(int valor) {
+        A1 = valor;
+    }
+
+    public void setA2(float valor) {
+        A2 = valor;
+    }
+
+    public void MA1() {
+        System.out.println("Método MA1() da classe A");
+    }
+
+    public void MA2() {
+        System.out.println("Método MA2() da classe A");
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB1(int valor) {
+        B1 = valor;
+    }
+
+    public void setB2(float valor) {
+        B2 = valor;
+    }
+
+    public void MB1() {
+        System.out.println("Método MB1() da classe B");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2() da classe B");
+    }
+}
