1ad8de18c173709a233eb72fce8dff8e0c690dd0
Gabriel Rogério Aparecido Paes Silva <gabriel.silva44@estudante.ufla.br>
2023-05-21 15:27:49 -0300

    Atualização de classes B e C, e criação de classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public String getD1() {
+        return D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    public void setD1(String valor) {
+        D1 = valor;
+    }
+
+    public void setD2(int valor) {
+        D2 = valor;
+    }
+
+    public void MD1() {
+        System.out.println("Método MD1() da classe D");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2() da classe D");
+    }
+}
